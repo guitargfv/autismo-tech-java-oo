680787cdb4c1cec899f245a687484fb1bd68deaf
gabrielfelix <gabrielfelix.guitar@gmail.com>
2021-07-18 11:26:50 -0300

    criando classe de teste de criacao de veiculo

---
+++ src/CriarVeiculo.java
@@ -0,0 +1,13 @@
+
+public class CriarVeiculo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Veiculo primeiroVeiculo = new Veiculo();
+		primeiroVeiculo.modelo = "gol";
+		System.out.println("Modelo do nosso primeiro Veiculo: " + primeiroVeiculo.modelo);
+
+	}
+
+}
